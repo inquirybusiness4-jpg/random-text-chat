1a4c93004cecadae451ed0df85f06d62487b6abe
inquirybusiness4-jpg <inquirybusiness4@gmail.com>
2026-01-07 02:42:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,72 @@
 const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
+const http = require("http");
+const { Server } = require("socket.io");
 
-let onlineUsers = 0;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static("public"));
+
+let onlineUsers = 0;
+let waitingUser = null;
+const pairs = new Map();
 
 io.on("connection", (socket) => {
   onlineUsers++;
   io.emit("online-users", onlineUsers);
 
+  socket.on("start", () => {
+    if (waitingUser && waitingUser !== socket.id) {
+      pairs.set(socket.id, waitingUser);
+      pairs.set(waitingUser, socket.id);
+
+      io.to(socket.id).emit("matched");
+      io.to(waitingUser).emit("matched");
+
+      waitingUser = null;
+    } else {
+      waitingUser = socket.id;
+      socket.emit("waiting");
+    }
+  });
+
+  socket.on("message", (msg) => {
+    const partner = pairs.get(socket.id);
+    if (partner) {
+      io.to(partner).emit("message", msg);
+    }
+  });
+
+  socket.on("typing", () => {
+    const partner = pairs.get(socket.id);
+    if (partner) io.to(partner).emit("typing");
+  });
+
+  socket.on("next", () => {
+    const partner = pairs.get(socket.id);
+    if (partner) {
+      pairs.delete(partner);
+      io.to(partner).emit("disconnected");
+    }
+    pairs.delete(socket.id);
+    waitingUser = socket.id;
+    socket.emit("waiting");
+  });
+
   socket.on("disconnect", () => {
     onlineUsers--;
     io.emit("online-users", onlineUsers);
+
+    const partner = pairs.get(socket.id);
+    if (partner) {
+      pairs.delete(partner);
+      io.to(partner).emit("disconnected");
+    }
+    pairs.delete(socket.id);
+
+    if (waitingUser === socket.id) waitingUser = null;
   });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+server.listen(3000, () => console.log("Server running"));
